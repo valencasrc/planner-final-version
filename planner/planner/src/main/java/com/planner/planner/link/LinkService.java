6ffbfd865a758be19ff9643f9abe564ce00c94e5
Lucas Valença <153026625+valencasrc@users.noreply.github.com>
2024-08-01 23:43:50 -0300

    Add files via upload

---
+++ planner/planner/src/main/java/com/planner/planner/link/LinkService.java
@@ -0,0 +1,33 @@
+package com.planner.planner.link;
+
+import com.planner.planner.activity.Activity;
+import com.planner.planner.activity.ActivityData;
+import com.planner.planner.activity.ActivityRequestPayload;
+import com.planner.planner.activity.ActivityResponse;
+import com.planner.planner.trip.Trip;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class LinkService {
+
+    @Autowired
+    private LinkRepository repository;
+
+    public LinkResponse registerLink(LinkRequestPayload payload, Trip trip){
+        Link newLink = new Link(payload.title(), payload.url(), trip);
+
+        this.repository.save(newLink);
+
+        return new LinkResponse(newLink.getId());
+    }
+
+    public List<LinkData> getAllLinksFromTrip(UUID tripId){
+        return this.repository.findByTripId(tripId).stream().map(link -> new LinkData(link.getId(), link.getTitle(), link.getUrl())).toList();
+    }
+
+}
